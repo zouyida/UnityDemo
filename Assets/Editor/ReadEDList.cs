3d761ecec1c19eadc0446cd745736911988e5476
zouyida <moliarty@163.com>
2023-01-13 05:25:40 +0800

    update [excel + unity]

---
+++ Assets/Editor/ReadEDList.cs
@@ -0,0 +1,40 @@
+using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+
+[InitializeOnLoad]
+public class ReadEDList
+{
+    static ReadEDList()
+    {
+        string filePath = Application.dataPath + "/Editor/EnemyDataList.xlsx";
+        string assetName = "EnemyDataList";
+        FileInfo fileInfo = new FileInfo(filePath);
+        EnemyDataList enemyDataList = (EnemyDataList)ScriptableObject.CreateInstance(typeof(EnemyDataList));
+        using (ExcelPackage excelPackage = new ExcelPackage(fileInfo))
+        {
+
+            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets["Enemy"];
+            for (int i = worksheet.Dimension.Start.Row + 2; i <= worksheet.Dimension.End.Row; i++)
+            {
+                EnemyData enemyData = new EnemyData();
+                Type type = typeof(EnemyData);
+                for (int j = worksheet.Dimension.Start.Column; j <= worksheet.Dimension.End.Column; j++)
+                {
+                    FieldInfo variable = type.GetField(worksheet.GetValue(2, j).ToString());
+                    string tableValue = worksheet.GetValue(i, j).ToString();
+                    variable.SetValue(enemyData, Convert.ChangeType(tableValue, variable.FieldType));
+                }
+                enemyDataList.enemyDataList.Add(enemyData);
+            }
+        }
+        AssetDatabase.CreateAsset(enemyDataList, "Assets/Resources/" + assetName + ".asset");
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Scripts/EnemyDataList.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EnemyType
+{
+    Normal,
+    Flyer,
+    Summoner
+};
+
+public class EnemyDataList : ScriptableObject
+{
+    public List<EnemyData> enemyDataList = new List<EnemyData>();
+}
+
+[System.Serializable]
+public class EnemyData
+{
+    public uint id;
+    public uint progressId;
+    public EnemyType enemyType;
+    public float spawnTime;
+    public int spawnPositionX;
+    public int spawnPositionY;
+}
